94f049593a7c5bbc7b6dd897fba93d1b71fbb4f5
speakingcode <2170844+speakingcode@users.noreply.github.com>
2026-03-31 11:22:38 -0500

    enhance test cases

--- src/12-useState-hook.spec.js
+++ src/12-useState-hook.spec.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { Greeting } from './12-useState-hook.js'
 
 describe ('Greeting', () => {
-  it('renders two forms', () => {
+  beforeEach(() => {
     render(<Greeting />)
+  })
+
+  it('renders two inputs with label "Name:"', () => {
     expect(screen.getAllByLabelText('Name:')).toHaveLength(2)
+  })
+
+  it('greets each name uniquely', async () => {
+    fireEvent.change(screen.getAllByLabelText('Name:')[0], {target: {value: 'john'}})
+    fireEvent.change(screen.getAllByLabelText('Name:')[1], {target: {value: 'doe'}})
+
+    const greeting1 = await screen.findByText('Hello john')
+    const greeting2 = await screen.findByText('Hello doe')
+
+    expect(greeting1).toBeDefined()
+    expect(greeting2).toBeDefined()
   })
 })
